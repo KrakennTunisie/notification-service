673bce3a8482b6f49a67a38cf965709fc01f24c3
OumaimaKouka <oumayma.chelly@kouka.io>
2026-08-11 17:38:12 +0200

    fix : fixing test security config

--- src/test/java/com/kerp/notificationservice/TestSecurityConfig.java
+++ src/test/java/com/kerp/notificationservice/TestSecurityConfig.java
@@ -1,17 +1,27 @@
 package com.kerp.notificationservice;
 
 
+import com.kerp.notificationservice.infrastructure.config.JwtConfig;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import static org.mockito.Mockito.mock;
 
+// TestSecurityConfig.java
 @TestConfiguration
+@ComponentScan(
+        basePackages = "com.kerp.notificationservice",
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = JwtConfig.class
+        )
+)
 public class TestSecurityConfig {
 
     @Bean
-    @Primary
     public JwtDecoder jwtDecoder() {
         return mock(JwtDecoder.class);
     }
