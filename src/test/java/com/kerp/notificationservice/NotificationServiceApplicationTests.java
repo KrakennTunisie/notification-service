97c962c84800b82b695a459d3253c0f9ea75258c
OumaimaKouka <oumayma.chelly@kouka.io>
2026-08-11 18:12:47 +0200

    fix : fixing test security config

--- src/test/java/com/kerp/notificationservice/NotificationServiceApplicationTests.java
+++ src/test/java/com/kerp/notificationservice/NotificationServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.kerp.notificationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 
 @SpringBootTest(properties = "spring.main.allow-bean-definition-overriding=true")
@@ -11,8 +13,10 @@ import org.springframework.test.context.ActiveProfiles;
 @Import(TestSecurityConfig.class)
 class NotificationServiceApplicationTests {
 
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
+
     @Test
     void contextLoads() {
     }
-
 }
