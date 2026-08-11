82804071efa5f11d9a51ab2f4a64dba7ab43b8e7
OumaimaKouka <oumayma.chelly@kouka.io>
2026-08-11 16:57:22 +0200

    fix: fixing jwt config

--- src/main/java/com/kerp/notificationservice/infrastructure/config/JwtConfig.java
+++ src/main/java/com/kerp/notificationservice/infrastructure/config/JwtConfig.java
@@ -1,5 +1,6 @@
 package com.kerp.notificationservice.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -8,11 +9,13 @@ import org.springframework.security.oauth2.jwt.JwtDecoders;
 @Configuration
 public class JwtConfig {
 
+    @Value("${keycloak.url}")
+    private String keycloak_url;
     @Bean
     JwtDecoder jwtDecoder() {
 
         return JwtDecoders.fromIssuerLocation(
-                "http://localhost:8080/realms/kerp"
+               keycloak_url
         );
     }
 }
